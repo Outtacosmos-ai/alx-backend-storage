07623ccc49fab907ec0e6bf586608952c2f57256
Outtacosmos-ai <messghir4@gmail.com>
2024-10-23 15:05:46 +0100

    done ✅

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,33 +1,27 @@
 #!/usr/bin/env python3
 """
-Python script that provides stats about Nginx logs stored in MongoDB
-Database: logs, Collection: nginx
+Provides some stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
 
 
 def print_nginx_logs_stats():
     """
-    Function that provides stats about Nginx logs stored in MongoDB
+    Provides some stats about Nginx logs stored in MongoDB
     """
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient()
     db = client.logs
-    nginx = db.nginx
+    collection = db.nginx
 
-    total_logs = nginx.count_documents({})
-    print(f"{total_logs} logs")
+    print(f"{collection.count_documents({})} logs")
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = nginx.count_documents({"method": method})
+        count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_check = nginx.count_documents({
-        "method": "GET",
-        "path": "/status"
-    })
-    print(f"{status_check} status check")
+    print(f"{collection.count_documents({'method': 'GET', 'path': '/status'})} status check")
 
 
 if __name__ == "__main__":
